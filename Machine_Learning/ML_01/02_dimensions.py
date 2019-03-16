c27bab458749edd998831dab2931610c90fd35e3
milgor931 <40010548+milgor931@users.noreply.github.com>
2019-03-16 13:23:34 -0700

    initial commit

---
+++ Machine_Learning/ML_01/02_dimensions.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+array = np.arange(10)
+
+newarray = array.reshape(2, 5)
+
+print(newarray)
---
+++ Machine_Learning/ML_01/03_dimensions.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+myList = np.array([[1, 2, 3, 4], [3, 4, 5, 6], [5, 6, 7, 8]])
+
+print("Shape: " + str(myList.shape))
+
+print("Data Type: " + str(myList.dtype))
+
+print("Size: " + str(myList.size))
+
+print("Number of Dimensions: " + str(myList.ndim))
